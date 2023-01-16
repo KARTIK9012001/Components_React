4f46d714364d22846a3e1b6cc604c2ecf1e04768
KARTIK9012001 <102713195+KARTIK9012001@users.noreply.github.com>
2023-01-16 15:45:07 +0530

    Add files via upload

---
+++ src/Comp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Comp(){
+   return <h2>Skoda</h2>
+   
+}
+export default Comp;
---
+++ src/Company.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Company(){
+   return <h1>Volkswagen</h1>
+   
+}
+export default Company;
